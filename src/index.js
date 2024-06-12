76fdc0e6ddaa90698baeff2184c859fa1f857577
Fernando Romero Marchante <159028132+Fernando-RomeroM@users.noreply.github.com>
2024-06-12 16:25:21 +0200

    Miercoles 12 medio dia

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const dotenv = require('dotenv');
+
+const port = process.env.PORT || 3000; // Usar la variable de entorno PORT si está definida ¿?
+
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
 
 // Configurar la carpeta 'public' como la carpeta estática
 app.use(express.static(path.join(__dirname, '../public')));
@@ -18,7 +24,10 @@ app.get('/', (req, res) => {
             <link rel="stylesheet" href="/styles.css">
         </head>
         <body>
-            <div id="feelcab"><img src="/images/firelogo.png" alt="Feel the Fire Logo"><h1>Feel the Fire</h1> <input type="text" id="busca" placeholder="Buscador" required>
+            <div id="feelcab">
+                <div id="feelcab1">
+                    <img src="/images/firelogo.png" id="imglogo" alt="Feel the Fire Logo"><h1>Feel the Fire</h1> <input type="text" id="busca" placeholder="Buscador" required>
+                </div>
                 <div id="menuser1">
                     <svg xmlns="http://www.w3.org/2000/svg" height="90" width="25" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffc547" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
                     <p>Login</p>
